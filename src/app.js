3ed1c601d24b246060dc8960891e5390562f5741
shivraj <dhotreshivraj206@gmail.com>
2024-03-21 18:06:51 +0530

    Made changes in cors

--- src/app.js
+++ src/app.js
@@ -6,10 +6,9 @@ import morgan from "morgan";
 const app = express()
 
 app.use(cors({
-    origin : "https://youtube-frontend-dle2.vercel.app",
-    credentials : true
-
-}))
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
